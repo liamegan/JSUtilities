ca77c188da6fd5bd01f1cf2bb47830ccfa4a5931
Liam Egan <liam@wethecollective.com>
2021-09-12 11:21:38 -0700

    Initial functions

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+declare const floatRandomBetween: (min: any, max: any) => any;
+declare const clamp: (a: any, b: any, v: any) => number;
+declare const lerp: (a: any, b: any, progress: any) => any;
+declare const hash21: (p: any, seed: any) => number;
+declare const precisionRound: (n: any, p: any) => number;
+declare const fract: (n: any, p?: number) => number;
+declare const xmur3: (t: any) => () => number;
+declare const asyncImageLoad: (t: any, e: any) => Promise<unknown>;
+declare const loadAllImages: (t: any) => Promise<any[]>;
+export { floatRandomBetween, clamp, lerp, hash21, precisionRound, fract, xmur3, asyncImageLoad, loadAllImages, };
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+
+const floatRandomBetween = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+const clamp = function (a, b, v) {
+  return Math.min(b, Math.max(a, v));
+};
+const lerp = function (a, b, progress) {
+  return a + progress * (b - a);
+};
+const hash21 = (p, seed) => {
+  const a = [27.609, 57.583];
+  if(p.array) p = p.array; // converting from wtc-math vec2 to array
+  const o = a[0] * p[0] + a[1] * p[1];
+  return fract(Math.sin(o) * seed);
+};
+const precisionRound = (n, p) => {
+  const ip = Math.pow(10, p);
+  return Math.round(n * ip) / ip;
+};
+const fract = (n, p = 6) => {
+  const ip = Math.pow(10, p);
+  const _n = Math.abs(Math.floor(n * ip) / ip);
+  if (_n < 1) return _n;
+  return Math.floor((_n % Math.floor(_n)) * ip) / ip;
+};
+
+const xmur3 = (t) => {
+  for (var e = 0, i = 1779033703 ^ t.length; e < t.length; e++)
+    i = ((i = Math.imul(i ^ t.charCodeAt(e), 3432918353)) << 13) | (i >>> 19);
+  return function () {
+    return (
+      (i = Math.imul(i ^ (i >>> 16), 2246822507)),
+      (i = Math.imul(i ^ (i >>> 13), 3266489909)),
+      (i ^= i >>> 16) >>> 0
+    );
+  };
+}
+const asyncImageLoad = function (t, e) {
+    return new Promise((i, s) => {
+      (t.onload = () => i(t)), (t.onerror = s), (t.src = e);
+    });
+  };
+const loadAllImages = function (t) {
+  const e = [];
+  for (let i in t) {
+    const s = new Image();
+    e.push(asyncImageLoad(s, t[i]));
+  }
+  return Promise.all(e);
+};
+
+export {
+  floatRandomBetween,
+  clamp,
+  lerp,
+  hash21,
+  precisionRound,
+  fract,
+  xmur3,
+  asyncImageLoad,
+  loadAllImages,
+};
